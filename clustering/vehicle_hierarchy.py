f07f1ecab3dc000a0c6325c27b0424a99c9158f3
Thanmai Reddy <43710388+thanmaireddy1997@users.noreply.github.com>
2020-01-24 21:12:06 -0600

    Add files via upload

---
+++ clustering/vehicle_hierarchy.py
@@ -0,0 +1,104 @@
+import numpy as np 
+import pandas as pd
+from scipy import ndimage 
+from scipy.cluster import hierarchy 
+from scipy.spatial import distance_matrix 
+from matplotlib import pyplot as plt 
+from sklearn import manifold, datasets 
+from sklearn.cluster import AgglomerativeClustering 
+from sklearn.datasets.samples_generator import make_blobs 
+
+filename = 'cars_clus.csv'
+
+#Read csv
+pdf = pd.read_csv(filename)
+print ("Shape of dataset: ", pdf.shape)
+
+pdf.head(5)
+
+print ("Shape of dataset before cleaning: ", pdf.size)
+pdf[[ 'sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']] = pdf[['sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']].apply(pd.to_numeric, errors='coerce')
+pdf = pdf.dropna()
+pdf = pdf.reset_index(drop=True)
+print ("Shape of dataset after cleaning: ", pdf.size)
+pdf.head(5)
+
+featureset = pdf[['engine_s',  'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap', 'mpg']]
+
+from sklearn.preprocessing import MinMaxScaler
+x = featureset.values #returns a numpy array
+min_max_scaler = MinMaxScaler()
+feature_mtx = min_max_scaler.fit_transform(x)
+feature_mtx [0:5]
+#scipy
+import scipy
+leng = feature_mtx.shape[0]
+D = scipy.zeros([leng,leng])
+for i in range(leng):
+    for j in range(leng):
+        D[i,j] = scipy.spatial.distance.euclidean(feature_mtx[i], feature_mtx[j])
+        
+import pylab
+import scipy.cluster.hierarchy
+Z = hierarchy.linkage(D, 'complete')
+
+
+from scipy.cluster.hierarchy import fcluster
+k = 5
+clusters = fcluster(Z, k, criterion='maxclust')
+clusters
+
+fig = pylab.figure(figsize=(18,50))
+def llf(id):
+    return '[%s %s %s]' % (pdf['manufact'][id], pdf['model'][id], int(float(pdf['type'][id])) )
+    
+dendro = hierarchy.dendrogram(Z,  leaf_label_func=llf, leaf_rotation=0, leaf_font_size =12, orientation = 'right')
+
+
+
+#scikit-Learn
+dist_matrix = distance_matrix(feature_mtx,feature_mtx) 
+print(dist_matrix)
+
+
+pdf['cluster_'] = agglom.labels_
+pdf.head()
+import matplotlib.cm as cm
+n_clusters = max(agglom.labels_)+1
+colors = cm.rainbow(np.linspace(0, 1, n_clusters))
+cluster_labels = list(range(0, n_clusters))
+
+# Create a figure of size 6 inches by 4 inches.
+plt.figure(figsize=(16,14))
+
+for color, label in zip(colors, cluster_labels):
+    subset = pdf[pdf.cluster_ == label]
+    for i in subset.index:
+            plt.text(subset.horsepow[i], subset.mpg[i],str(subset['model'][i]), rotation=25) 
+    plt.scatter(subset.horsepow, subset.mpg, s= subset.price*10, c=color, label='cluster'+str(label),alpha=0.5)
+#    plt.scatter(subset.horsepow, subset.mpg)
+plt.legend()
+plt.title('Clusters')
+plt.xlabel('horsepow')
+plt.ylabel('mpg')
+
+pdf.groupby(['cluster_','type'])['cluster_'].count()
+
+agg_cars = pdf.groupby(['cluster_','type'])['horsepow','engine_s','mpg','price'].mean()
+agg_cars
+
+plt.figure(figsize=(16,10))
+for color, label in zip(colors, cluster_labels):
+    subset = agg_cars.loc[(label,),]
+    for i in subset.index:
+        plt.text(subset.loc[i][0]+5, subset.loc[i][2], 'type='+str(int(i)) + ', price='+str(int(subset.loc[i][3]))+'k')
+    plt.scatter(subset.horsepow, subset.mpg, s=subset.price*20, c=color, label='cluster'+str(label))
+plt.legend()
+plt.title('Clusters')
+plt.xlabel('horsepow')
+plt.ylabel('mpg')
+
